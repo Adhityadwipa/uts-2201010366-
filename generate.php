76826142b6aad5869b3dc8935ba104175ae161fa
Adhitya <127808855+Adhityadwipa@users.noreply.github.com>
2023-05-22 03:33:11 -0800

    Update generate.php

--- generate.php
+++ generate.php
@@ -44,10 +44,7 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-book-open"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Bersama</div>
+                <div class="sidebar-brand-text mx-3"><h2 class="font-weight-bold">Bersama</h2></div>
             </a>
 
             <!-- Divider -->
